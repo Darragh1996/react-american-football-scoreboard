1274a5198eb7727909d16dc92aecc18ae5efd0e7
Darragh Ferry <darraghferry@hotmail.com>
2019-11-04 15:10:23 +0000

    seperated top row into its own component

--- src/App.js
+++ src/App.js
@@ -3,23 +3,29 @@ import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
-function ScoreBoard(props) {
+function TopRow(props){
   return (
-    <section className="scoreboard">
-      <div className="topRow">
+    <div className="topRow">
         <div className="home">
           <h2 className="home__name">Lions</h2>
 
           {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-          <div className="home__score">{props.scores.homeScore}</div>
+          <div className="home__score">{props.scores.scores.homeScore}</div>
         </div>
         <div className="timer">00:03</div>
         <div className="away">
           <h2 className="away__name">Tigers</h2>
-          <div className="away__score">{props.scores.awayScore}</div>
+          <div className="away__score">{props.scores.scores.awayScore}</div>
         </div>
       </div>
+  )
+}
+
+function ScoreBoard(props) {
+  return (
+    <section className="scoreboard">
+      <TopRow scores={props}/>
       <BottomRow />
     </section>
   );
